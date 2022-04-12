f2ad98a2ac5559eca4bf00736178ca438b217944
Miguel <jemiguelrivas@gmail.com>
2022-04-12 12:28:41 -0400

    fixing translation files

--- src/db/projects.js
+++ src/db/projects.js
@@ -13,7 +13,7 @@ const linkVimeo = (id) => `https://player.vimeo.com/video/${id}`;
 const projects = [
 
   {
-    date: "2021/04/11",
+    date: "2022/04/11",
     title: "Portfolio 2022-04",
     types: type.app,
     clients: [
