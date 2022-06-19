0414ea64f18a5b6a82756a7212c20b0d46c5f625
Miguel <jemiguelrivas@gmail.com>
2022-06-19 10:58:31 -0400

    export spirit component to nano spirit

--- src/components/spirit.vue
+++ src/components/spirit.vue
@@ -5,66 +5,9 @@
 
 <script>
 import Vue from "vue";
-
-function spirit(word, klaso) {
-  return `<span class='sp-${klaso}'>${word}</span>`;
-}
-
-function spiritBegin(klaso) {
-  return `<span class='sp-${klaso}'>`;
-}
-
-function spiritEnd() {
-  return `</span>`;
-}
-
-function pugFormat(text) {
-  return text
-    .replace(/\n/g, "<br>")
-    .replace(/\s/g, "&nbsp;")
-
-    .replace(/(\=\")/g, spirit("&#61;&#34;", "quote") + spiritBegin("value"))
-    .replace(/(\")/g, spiritEnd() + spirit("&#34;", "quote"))
-    .replace(/(?<=&lt;[a-z]{1,})&nbsp;/g, "&nbsp;" + spiritBegin("attr"))
-
-    .replace(/\(/g, spirit("(", "tag") + spiritBegin("attr"))
-    .replace(/\)/g, spiritEnd() + spirit(")", "tag"));
-}
-
-function hamlFormat(text) {
-  return text
-    .replace(/\n/g, "<br>")
-    .replace(/\s/g, "&nbsp;")
-
-    .replace(
-      /(\:&nbsp;\")/g,
-      spirit(":&nbsp;&#34;", "quote") + spiritBegin("value")
-    )
-    .replace(/(\")/g, spiritEnd() + spirit("&#34;", "quote"))
-    .replace(/(?<=&lt;[a-z]{1,})&nbsp;/g, "&nbsp;" + spiritBegin("attr"))
-
-    .replace(/\{/g, spirit("{", "tag") + spiritBegin("attr"))
-    .replace(/\}/g, spiritEnd() + spirit("}", "tag"));
-}
-
-function htmlFormat(text) {
-  return text
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/\//g, "&#47;")
-
-    .replace(/\n/g, "<br>")
-    .replace(/\s/g, "&nbsp;")
-
-    .replace(/(\=\")/g, spirit("&#61;&#34;", "quote") + spiritBegin("value"))
-    .replace(/(\")/g, spiritEnd() + spirit("&#34;", "quote"))
-    .replace(/(?<=&lt;[a-z]{1,})&nbsp;/g, "&nbsp;" + spiritBegin("attr"))
-
-    .replace(/(?<=&lt;)&#47;/g, spirit("&#47;", "tag"))
-    .replace(/&lt;/g, spirit("&lt;", "tag"))
-    .replace(/&#47;(?=&gt;)/g, spiritEnd() + spirit("&#47;", "tag"))
-    .replace(/&gt;/g, spiritEnd() + spirit("&gt;", "tag"));
-}
+import { pug } from "nano-spirit/module/code-pug";
+import { haml } from "nano-spirit/module/code-haml";
+import { html } from "nano-spirit/module/code-html";
 
 export default Vue.extend({
   props: {
@@ -80,11 +23,11 @@ export default Vue.extend({
   computed: {
     textFormat() {
       if (this.lang === "pug") {
-        return pugFormat(this.text);
+        return pug(this.text);
       } else if (this.lang === "haml") {
-        return hamlFormat(this.text);
-      } else {
-        return htmlFormat(this.text);
+        return haml(this.text);
+      } else if (this.lang === "html") {
+        return html(this.text);
       }
     },
   },
